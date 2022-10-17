bd2a55e1b21b088657cb49d811fef6d4d9f9a838
sviat-l <sviatoslav.lushnei@ucu.edu.ua>
2022-10-17 17:28:37 +0300

    add data

---
+++ lab1.r
@@ -0,0 +1,10 @@
+library(tidytext)
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+list.files(getwd())
+list.files("2-fakeNews")
+
+test_path <- "2-fakeNews/test.csv"
+train_path <- "2-fakeNews/train.csv"
