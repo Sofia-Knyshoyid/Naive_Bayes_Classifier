284bad781a2fafffaf2b692be631e36667c5048a
Sofia-Knyshoyid <sofiaknyshoid@gmail.com>
2024-03-25 01:24:03 +0200

    add files

---
+++ MyTest.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+class MyTest {
+    public static void main(String[] args) {
+//        ArrayList<Object> al4 = new ArrayList<String>();
+    }
+}
---
+++ sth.R
@@ -0,0 +1,6 @@
+# Create fruit vector
+fruit <- c('Apple', 'Orange', 'Passion fruit', 'Banana')
+# Create the for statement
+for ( i in fruit){ 
+ print(i)
+}
